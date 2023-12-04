673f1ce5e368bf6326b04874ecd03eadb563efba
Mudasar Ali <mudasarali88@gmail.com>
2023-12-04 08:20:20 -0500

    remove db error

--- app/Models/HomeModel.php
+++ app/Models/HomeModel.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class HomeModel extends Model
 {
     use HasFactory;
-    protected $table = 'homepage';
+    protected $table = 'homePage';
     protected $fillable = ['body', 'user_id', 'mataTitle', 'mataDescription', 'mataTags'];
 }
